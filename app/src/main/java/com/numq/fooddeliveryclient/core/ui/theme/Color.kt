2297774396c119479d85315c00830779343fa6ef
numq <numq.dev@gmail.com>
2022-07-06 21:44:12 +0300

    Fix dependencies

---
+++ app/src/main/java/com/numq/fooddeliveryclient/core/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.numq.fooddeliveryclient.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Jet = Color(0xFF333232)
+val Melon = Color(0xFFF7B2AD)
+val SilverPink = Color(0xFFCEB7B3)
+val GrayX11Gray = Color(0xFFB7B7B7)
