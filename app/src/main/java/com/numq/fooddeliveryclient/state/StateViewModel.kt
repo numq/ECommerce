e45d4fbe44138d277e174d725233e7f77f1b4ceb
numq <numq.dev@gmail.com>
2022-11-30 16:26:46 +0300

    Add base vm

---
+++ app/src/main/java/com/numq/fooddeliveryclient/state/StateViewModel.kt
@@ -0,0 +1,28 @@
+package com.numq.fooddeliveryclient.state
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+abstract class StateViewModel<T> constructor(initialValue: T) : ViewModel() {
+
+    private val _state: MutableStateFlow<T> = MutableStateFlow(initialValue)
+    val state: StateFlow<T> = _state.asStateFlow()
+
+    private val _exception: Channel<Exception?> = Channel()
+    val exception: Flow<Exception?> = _exception.consumeAsFlow()
+
+    val onException: (Exception) -> Unit = { viewModelScope.launch { _exception.send(it) } }
+
+    val initialize: () -> (() -> Unit) -> Unit = { { viewModelScope.launch { it() } } }
+
+    fun updateState(f: (T) -> T) {
+        _state.update { f(it) }
+    }
+
+    init {
+        initialize()
+    }
+}
