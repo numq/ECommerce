3d284e551761ef4a14609f473567fde4a1ac48c2
numq <numq.dev@gmail.com>
2022-12-18 13:36:52 +0300

    Add wrapper

--- app/src/main/java/com/numq/fooddeliveryclient/wrapper/T.kt
+++ app/src/main/java/com/numq/fooddeliveryclient/wrapper/T.kt
@@ -2,15 +2,8 @@ package com.numq.fooddeliveryclient.wrapper
 
 import arrow.core.Either
 import arrow.core.flatten
-import arrow.core.left
-import arrow.core.right
 
-fun <T> T.wrap(): Either<Exception, T> =
-    runCatching { this }
-        .fold(
-            onSuccess = { it.right() },
-            onFailure = { Exception(it.message).left() }
-        )
+fun <T> T.wrap(): Either<Exception, T> = Either.catch { this }.mapLeft { Exception(it.message) }
 
 fun <T> T.wrapIf(condition: Boolean, exception: Exception): Either<Exception, T> =
     Either.conditionally(
