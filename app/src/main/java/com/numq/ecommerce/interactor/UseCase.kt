04c3713d09f2cbca93f7e27e7da4c9e7d292ed03
numq <numq.dev@gmail.com>
2023-02-16 00:59:16 +0300

    Provide refactoring

---
+++ app/src/main/java/com/numq/ecommerce/interactor/UseCase.kt
@@ -0,0 +1,21 @@
+package com.numq.ecommerce.interactor
+
+import arrow.core.Either
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+abstract class UseCase<in T, out R> {
+
+    abstract suspend fun execute(arg: T): Either<Exception, R>
+
+    operator fun invoke(
+        coroutineScope: CoroutineScope,
+        arg: T,
+        onException: (Exception) -> Unit,
+        onResult: (R) -> Unit = {}
+    ) {
+        coroutineScope.launch {
+            execute(arg).fold(onException, onResult)
+        }
+    }
+}
